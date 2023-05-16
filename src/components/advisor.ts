db76cf1a46ebaea053bb45050960612306a0c49a
Nitin Ganwani <nitganwani@gmail.com>
2023-05-16 22:00:24 +0200

    Change properties for advisor and squire characters

--- src/components/advisor.ts
+++ src/components/advisor.ts
@@ -1,4 +1,7 @@
 import { Character } from '../models/character';
+import { Fighter } from './fighter';
+import { King } from './king';
+import { Squire } from './squire';
 
 export class Advisor extends Character {
   advisedCharacter;
@@ -6,10 +9,43 @@ export class Advisor extends Character {
     name: string,
     house: string,
     age: number,
-    advisedCharacter: string
+    advisedCharacter: King | Squire | Fighter
   ) {
     super(name, house, age);
     this.advisedCharacter = advisedCharacter;
     this.speech = 'No sé por qué, pero creo que voy a morir pronto';
   }
 }
+
+/* `<li class="character col">
+    <div class="card character__card">
+      <img src="/joffrey.jpg" alt="Joffrey Baratheon" class="character__picture card-img-top" />
+      <div class="card-body">
+        <h2 class="character__name card-title h4"> </h2>
+        <div class="character__info">
+          <ul class="list-unstyled">
+            <li>Edad:  años</li>
+            <li>
+              Estado:
+              <i class="fas fa-thumbs-down"></i>
+              <i class="fas fa-thumbs-up"></i>
+            </li>
+          </ul>
+        </div>
+        <div class="character__overlay">
+          <ul class="list-unstyled">
+            <li>Años de reinado: </li>
+          </ul>
+          <div class="character__actions">
+            <button class="character__action btn">habla</button>
+            <button class="character__action btn">muere</button>
+          </div>
+        </div>
+      </div>
+      <i class="emoji">👑</i>
+    </div>
+  </li>
+  <div class="comunications">
+      <p class="comunications__text display-1">Una frase que dice alguien</p>
+      <img class="comunications__picture" src="/public/no-one.jpg" alt="Nombre y familia del que habla" />
+    </div>`; */
